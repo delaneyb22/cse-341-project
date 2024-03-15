7aa086767c5e87bc019dc8e85f07e5ff71b77050
Delaney Baker <114117065+delaneyb22@users.noreply.github.com>
2024-03-14 19:10:51 -0500

    add url from render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title:'Users Api',
         description: 'Users Api'
     },
-    host: 'localhost:3000',
-    schemes:['https', 'http']
+    host: 'localhost:3001',
+    schemes:['http', 'https']
 };
 
 const outputFile = './swagger.json'
